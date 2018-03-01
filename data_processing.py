fixed some bugs in the outliers computation

data_processing.py
@@ -113,10 +113,11 @@ class DataProcessing:
         for ((gpsb, gpse), j) in zip(self.segments, xrange(self.nsegments)):
             aux_data = self.get_channel_data(data_source,
                                              aux_channel, gpsb, gpse)
-            min_thr, max_thr = np.percentile(aux_data, [1 - self.outliers_quantile,
-                                                        self.outliers_quantile])
-            bad_idxs = np.where(np.logical_or((aux_data < min_thr),
-                                              (aux_data > max_thr)))
+            min_thr, max_thr = np.percentile(aux_data,
+                                             [100 * (1 - self.outliers_quantile),
+                                              100 * self.outliers_quantile])
+            bad_idxs = np.nonzero(np.logical_or((aux_data < min_thr),
+                                              (aux_data > max_thr)))[0]
             total_points += len(aux_data) - len(bad_idxs)
             id_segments = []
             previous_bad = -1